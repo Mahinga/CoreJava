5fc4d98f5c8537ada17151314f02f30fc45b3726
Mahinga <mahinga.cheema@gmail.com>
2017-08-27 22:22:15 +0530

    Code changes done for Adapter Pattern

---
+++ src/designPatterns/structuralDesignPatterns/adaptorPattern/TestAdapterPattern.java
@@ -0,0 +1,20 @@
+/**
+ * This is test file created by developer Mahinga Singh in public interest.
+ */
+package designPatterns.structuralDesignPatterns.adaptorPattern;
+
+/**
+ * @author MAHINGA
+ *
+ */
+public class TestAdapterPattern {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
