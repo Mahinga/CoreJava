60680f24c2550cd62c03a438b04273f50c7e86c2
mahsingh-qasource <mahsingh@qasource.com>
2017-08-29 21:01:40 +0530

    flyweight design pattern

---
+++ src/designPatterns/structuralDesignPatterns/flyweightPattern/Shape.java
@@ -0,0 +1,6 @@
+package designPatterns.structuralDesignPatterns.flyweightPattern;
+
+public interface Shape {
+    public void draw(Graphics g, int x, int y, int width, int height, Color color);
+
+}
